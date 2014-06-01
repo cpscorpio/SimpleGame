73011f476ada0e022e812be04574dc30278b2936
Chen Peng <cpscorpio@163.com>
2014-06-01 21:14:49 +0800

    refreshResIndex

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 cc.game.onStart = function(){
-    cc.view.setDesignResolutionSize(800, 450, cc.ResolutionPolicy.SHOW_ALL);
-	cc.view.resizeWithBrowserSize(true);
+    cc.view.setDesignResolutionSize(960, 640, cc.ResolutionPolicy.SHOW_ALL);
+    cc.view.resizeWithBrowserSize(true);
     //load resources
     cc.LoaderScene.preload(g_resources, function () {
         cc.director.runScene(new HelloWorldScene());
     }, this);
 };
+
+var isMobile=true;
+
+var isDemo = true;
+
 cc.game.run();
